6ddb53352b5d8b00d9d8542db901c6fe3b98f18c
vbrh-immalle <hans.vanbroeckhoven@immalle.be>
2016-09-16 05:13:21 +0200

    Watch out with special regex character .

--- tests.js
+++ tests.js
@@ -11,6 +11,32 @@ QUnit.test("Simple regex", function(assert) {
     assert.equal(re.exec(str), exc, "Execution of regexp returns " + exc);
 });
 
+QUnit.test("Simple regex: . doesn't stand for dot", function(assert) {
+    var re = /www.google.com/;
+    var str = "http://www.google.com";
+    var exc = "www.google.com";
+
+    assert.ok(re.exec(str) !== null, "Execution of regexp returns something.");
+    assert.equal(re.exec(str), exc, "Execution of regexp returns " + exc);
+
+    str = "http://wwwXgoogleXcom";
+    exc = "wwwXgoogleXcom";
+    assert.ok(re.exec(str) !== null, "Execution of regexp should catch " + str);
+    assert.equal(re.exec(str), exc, "Execution of regexp returns " + exc);
+});
+
+QUnit.test("Simple regex: \. stands for dot", function(assert) {
+    var re = /www\.google\.com/;
+    var str = "http://www.google.com";
+    var exc = "www.google.com";
+
+    assert.ok(re.exec(str) !== null, "Execution of regexp returns something.");
+    assert.equal(re.exec(str), exc, "Execution of regexp returns " + exc);
+
+    str = "http://wwwXgoogleXcom";
+    assert.ok(re.exec(str) === null, "Execution of regexp shouldn't catch " + str);
+});
+
 QUnit.test("Http or https", function(assert) {
     var re = /http[s]?/;
     var str = "http://www.google.com";
