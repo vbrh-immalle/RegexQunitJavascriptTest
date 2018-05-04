9847dd02dbd4c14209699b6c3281674e64d28d57
vbrh-immalle <hans.vanbroeckhoven@immalle.be>
2018-05-04 21:29:46 +0200

    Use exp in stead of exc as short for expectation

--- tests.js
+++ tests.js
@@ -5,89 +5,89 @@ QUnit.test("hello test", function(assert) {
 QUnit.test("Simple regex", function(assert) {
     var re = /http/;
     var str = "http://www.google.com";
-    var exc = "http";
+    var exp = "http";
 
     assert.ok(re.exec(str) !== null, "Execution of regexp returns something.");
-    assert.equal(re.exec(str), exc, "Execution of regexp returns " + exc);
+    assert.equal(re.exec(str), exp, "Execution of regexp returns " + exp);
 });
 
 QUnit.test("Simple regex: . doesn't stand for dot", function(assert) {
     var re = /www.google.com/;
     var str = "http://www.google.com";
-    var exc = "www.google.com";
+    var exp = "www.google.com";
 
     assert.ok(re.exec(str) !== null, "Execution of regexp returns something.");
-    assert.equal(re.exec(str), exc, "Execution of regexp returns " + exc);
+    assert.equal(re.exec(str), exp, "Execution of regexp returns " + exp);
 
     str = "http://wwwXgoogleXcom";
-    exc = "wwwXgoogleXcom";
+    exp = "wwwXgoogleXcom";
     assert.ok(re.exec(str) !== null, "Execution of regexp should catch " + str);
-    assert.equal(re.exec(str), exc, "Execution of regexp returns " + exc);
+    assert.equal(re.exec(str), exp, "Execution of regexp returns " + exp);
 });
 
 QUnit.test("Simple regex: \. stands for dot", function(assert) {
     var re = /www\.google\.com/;
     var str = "http://www.google.com";
-    var exc = "www.google.com";
+    var exp = "www.google.com";
 
     assert.ok(re.exec(str) !== null, "Execution of regexp returns something.");
-    assert.equal(re.exec(str), exc, "Execution of regexp returns " + exc);
+    assert.equal(re.exec(str), exp, "Execution of regexp returns " + exp);
 
-    str = "http://wwwXgoogleXcom";
+    str = "http://wwwXgooglexpom";
     assert.ok(re.exec(str) === null, "Execution of regexp shouldn't catch " + str);
 });
 
 QUnit.test("Http or https", function(assert) {
     var re = /http[s]?/;
     var str = "http://www.google.com";
-    var exc = "http";
+    var exp = "http";
 
     assert.ok(re.exec(str) !== null, "Execution of regexp returns something.");
-    assert.equal(re.exec(str), exc, "Execution of regexp returns " + exc);
+    assert.equal(re.exec(str), exp, "Execution of regexp returns " + exp);
 
     str = "https://www.google.com";
-    exc = "https";
+    exp = "https";
 
     assert.ok(re.exec(str) !== null, "Execution of regexp returns something.");
-    assert.ok(re.exec(str) == exc, "Execution of regexp returns " + exc);
+    assert.ok(re.exec(str) == exp, "Execution of regexp returns " + exp);
 });
 
 QUnit.test("Http[s]?://", function(assert) {
     var re = /http[s]?:\/\//;
     var str = "http://www.google.com";
-    var exc = "http://";
+    var exp = "http://";
 
     assert.ok(re.exec(str) !== null, "Execution of regexp returns something.");
-    assert.equal(re.exec(str), exc, "Execution of regexp return " + exc);
+    assert.equal(re.exec(str), exp, "Execution of regexp return " + exp);
 
     str = "https://www.google.com";
-    exc = "https://";
+    exp = "https://";
 
     assert.ok(re.exec(str) !== null, "Execution of regexp returns something.");
-    assert.equal(re.exec(str), exc, "Execution of regexp return " + exc);
+    assert.equal(re.exec(str), exp, "Execution of regexp return " + exp);
 });
 
 QUnit.test("Should catch Github username", function(assert) {
     var re = /http[s]?:\/\/github\.com\/(\w+)/;
     var str = "http://github.com/testuser";
-    var exc = "http://github.com/testuser";
+    var exp = "http://github.com/testuser";
     var usr = "testuser";
 
     var resultArray = re.exec(str);
 
-    assert.equal(resultArray[0], exc, "Execution of regexp returns " + exc);
+    assert.equal(resultArray[0], exp, "Execution of regexp returns " + exp);
     assert.equal(resultArray[1], usr, "Username is parsed correctly as " + usr);
 });
 
 QUnit.test("Should catch Github username containing dashs and numbers", function(assert) {
     var re = /http[s]?:\/\/github\.com\/([\w-]+)/;
     var str = "http://github.com/3test-user";
-    var exc = "http://github.com/3test-user";
+    var exp = "http://github.com/3test-user";
     var usr = "3test-user";
 
     var resultArray = re.exec(str);
 
-    assert.equal(resultArray[0], exc, "Execution of regexp returns " + exc);
+    assert.equal(resultArray[0], exp, "Execution of regexp returns " + exp);
     assert.equal(resultArray[1], usr, "Username is parsed correctly as " + usr);
 
     // NOTE: only single hyphens are allowed (https://github.com/regexhq/regex-username)
